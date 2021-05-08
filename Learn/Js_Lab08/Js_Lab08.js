85e652c982f63b7ece4dac2a5d0516c7e7a69d92
Chanwit <73813114+Chanwir@users.noreply.github.com>
2021-05-08 11:35:13 +0700

    Create Js_Lab08.js

---
+++ Learn/Js_Lab08/Js_Lab08.js
@@ -0,0 +1,4 @@
+let txt = prompt('Enter Inner text to Show')
+document.querySelector("#p1").innerText = txt
+let txt2 = prompt('Enter Inner text2 to Show')
+document.querySelector("#p2").innerText = txt2
