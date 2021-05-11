4eb37332d6c30e1f5d84f546b857247be69f84f0
Chanwit <73813114+Chanwir@users.noreply.github.com>
2021-05-11 12:24:06 +0700

    Exercise Function 23_V2

---
+++ Java_Script/Basic/Exercise Function/23 V.2/23_V2.js
@@ -0,0 +1,28 @@
+function draw(n) {
+    for (let row = 0; row < (2 * n - 1); row++) {
+        let num = "";
+        for (let colum = 0; colum < (2 * n - 1); colum++) {
+            if (colum<(n-1) && row<(n-1) && (row+colum)<(n-1)) {
+                num += `-`
+            }
+            else if (colum<(n-1) && row>(n-1) && (row+n-colum-1)>(2*n-2)) {
+                num += `-`
+            }
+            else if (colum>(n-1) && row<(n-1) && (colum+n-row-1)>(2*n-2)) {
+                num += `-`
+            }
+            else if (colum>(n-1) && row>(n-1) && (row+colum)>(3*n-3)) {
+                num += `-`
+            }
+            else {
+                num += `*`
+            }
+        }
+        console.log(num);
+        document.querySelector("#p1").innerHTML += `${num}<br>`
+
+    }
+}
+
+let a = +prompt("Input Your Number :");
+draw(a);
